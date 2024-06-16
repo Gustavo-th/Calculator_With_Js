72725cd9f3a0c94fb65bccf3ed91c352148f5f21
Gustavo-th <gncampos2017@gmail.com>
2024-06-16 10:57:37 -0300

    initial-commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+
+
+function clearDisplay(){
+    display.value = '';
+
+}
+
+
+
+function calculate(){
+    try {
+        display.value = new Function('return ' + display.value) ();
+    }
+    catch(error) {
+        display.value = 'Error';
+    }
+}
+console.log(display.value);
